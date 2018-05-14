3aea7db66aa2d6bb89ae3f7280e7f030d04344eb
anderskristo <anderskristo@gmail.com>
2018-05-14 10:20:03 +0200

    Clean dialogue

--- dialogue.js
+++ dialogue.js
@@ -1,22 +1,25 @@
 
-// gränsnittet för kod under stationen
+// Gränsnittet för kod under stationen
 // Frågorna fylls i under stationsaktivitet
 // TODO: bestäm startmallformat för stationen + konsekens och samma språk igenom denna sida + övriga förbättringar vi hinner med.
-//
-var introText = "Här skrivs en hälsning";
 
+var introText = "Här skrivs en hälsning";
 var otydligText = "Här skrivs svar på okänd fråga %s";
 
-var intents = [{
+var intents = [
+
+  {
     intent: "enkel fråga",
     response: "Direkt svar"
   },
+
   {
     intent: "fråga med svar",
     response: "Prompt för input",
     action: (results) => `Svar med input: ${results.response}`
   }
-]
+
+];
 
 module.exports = {
   introText,
